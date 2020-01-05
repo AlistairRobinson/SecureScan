78f52d92dc84c756fb049c1b40646838a8fdc309
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2020-01-05 17:50:38 +0000

    Implement entropy and Levenshtein distance

--- simulation/__main__.py
+++ simulation/__main__.py
@@ -1,12 +1,91 @@
 from simulation.actors import Station, AccessPoint
+from simulation.frames import Frame, FrameType
+from scipy.stats import entropy
+from matplotlib import pyplot as plt
+import matplotlib
+import numpy as np
+import argparse, random, Levenshtein
 
-s = Station()
-a = AccessPoint("Example")
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", help = "operates simulation in verbose mode", action = 'store_true')
+parser.add_argument("-p", help = "plots data to a given file")
+parser.add_argument("-n", help = "the number of iterations to perform")
+parser.add_argument("-s", help = "the number of stations to simulate")
+parser.add_argument("-a", help = "the number of access points to simulate")
+args = parser.parse_args()
 
-beacon = a.send_beacon()
-print(beacon)
-request = s.send_probe_request(beacon)
-print(request)
-response = a.send_probe_response(request)
-print(response)
-assert s.verify_probe_response(response)
+n = 100
+s = 1
+a = 1
+p = ""
+v = False
+
+if args.v:
+    v = True
+if args.p:
+    p = args.p
+if args.n:
+    n = int(args.n)
+if args.s:
+    s = int(args.s)
+if args.a:
+    a = int(args.a)
+
+print("Beginning simulation with %d stations, %d access points, %d repetitions" % (s, a, n))
+
+stations = [Station() for i in range(0, s)]
+aps = [AccessPoint("AP" + str(i)) for i in range(0, a)]
+
+history = []
+
+for i in range(0, n):
+    st = random.choice(stations)
+    ap = random.choice(aps)
+    beacon = ap.send_beacon()
+    request = st.send_probe_request(beacon)
+    response = ap.send_probe_response(request)
+    assert st.verify_probe_response(response)
+    history.append(beacon)
+    history.append(request)
+    history.append(response)
+
+dist = [[] for i in range(0, 10000)]
+
+l = 0
+m = 0
+n = 0
+for f in history:
+    i = 0
+    if f.type != FrameType['ProbeRequest']:
+        continue
+    for c in str(f.contents):
+        dist[i].append(c)
+        i += 1
+    for h in history:
+        if f != h and f.type == h.type:
+            assert f.source != h.source
+            assert f.contents != h.contents
+            d = 0 # Levenshtein.distance(str(f.contents), str(h.contents))
+            if d < m or m == 0:
+                m = d
+            l += d
+            n += 1
+
+entropies = []
+
+for d in dist:
+    if d == []:
+        continue
+    values, counts = np.unique(d, return_counts=True)
+    entropies.append(entropy(counts))
+
+plt.plot(entropies)
+plt.show()
+
+if p != "":
+    plt.savefig(p)
+
+print("All simulations completed, no anomalies")
+print("Average Levenshtein distance: \t%d" % (l / n))
+print("Minimum Levenshtein distance: \t%d" % m)
+print("Total entropy: \t\t\t%d" % sum(entropies))
