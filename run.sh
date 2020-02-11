fb1710c4db2f2c650804beb5467005a913d86503
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2020-02-11 11:15:18 +0000

    Correct protocol naming scheme

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-for p in "secure_scan" "standard"
+for protocol in "secure_scan" "standard"
 do
     for s in 10 100 1000 10000
     do
@@ -6,8 +6,8 @@ do
         do
             for p in 0.0001 0.001 0.01 0.1
             do
-                echo "python3 -m simulation --csv results.csv -n 10000 -b --protocol $p -s $s -a $a -p $p"
-                python3 -m simulation --csv results.csv -n 10000 -b --protocol $p -s $s -a $a -p $p
+                echo "python3 -m simulation --csv results.csv -n 10000 -b --protocol $protocol -s $s -a $a -p $p"
+                python3 -m simulation --csv results.csv -n 10000 -b --protocol $protocol -s $s -a $a -p $p
             done
         done
     done
