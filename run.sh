16d13962d1f1fece203195ce64a419ee9550497c
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2020-02-11 10:46:55 +0000

    Add automated result collection script

---
+++ run.sh
@@ -0,0 +1,14 @@
+for p in "secure_scan" "standard"
+do
+    for s in 10 100 1000 10000
+    do
+        for a in 10 100 1000 10000
+        do
+            for p in 0.0001 0.001 0.01 0.1
+            do
+                echo "python3 -m simulation --csv results.csv -n 10000 -b --protocol $p -s $s -a $a -p $p"
+                python3 -m simulation --csv results.csv -n 10000 -b --protocol $p -s $s -a $a -p $p
+            done
+        done
+    done
+done
