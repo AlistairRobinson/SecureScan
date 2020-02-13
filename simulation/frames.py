cfd79d46d896d8904150e12fe2b2e0e68e3d1721
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2020-02-13 22:03:30 +0000

    Add frame docstrings

--- simulation/frames.py
+++ simulation/frames.py
@@ -2,13 +2,25 @@ from datetime import datetime
 from enum import Enum
 
 class FrameType(Enum):
+    """ Represents the enumerable types of a frame in the network
+    """
     Beacon = 1
     ProbeRequest = 2
     ProbeResponse = 3
 
 class Frame:
+    """ Represents a Frame in a WiFi network
 
-    def __init__(self, t:FrameType, src:str, dest:str, uid:int, data):
+    Attributes:
+        sent_at (datetime): The time at which the Frame was sent
+        type (FrameType):   The Frame's type (see FrameType)
+        source (str):       The Frame's source MAC address
+        destination (str):  The Frame's destination MAC address
+        uid (int):          The Frame's unique identifier in the simulation
+        contents:           The Frame's contents or payload
+    """
+
+    def __init__(self, t: FrameType, src: str, dest: str, uid: int, data):
         self.sent_at = datetime.now()
         self.type = t
         self.source = src
@@ -22,5 +34,5 @@ class Frame:
         "Source: \t{}\n" \
         "Destination: \t{}\n" \
         "Sent by actor {}\n" \
-        "Contents: \n{}".format(self.type, self.sent_at,
-        self.source, self.destination, self.uid, self.contents)
+        "Contents: \n{}".format(self.type, self.sent_at, self.source,
+                                self.destination, self.uid, self.contents)
