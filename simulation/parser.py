2048f85d9c5b5ba62bcba7751dfa2a30cb0516c7
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2020-02-13 22:06:05 +0000

    Add parser docstring

--- simulation/parser.py
+++ simulation/parser.py
@@ -1,12 +1,17 @@
 import argparse
 
-def construct_parser():
+def construct_parser() -> argparse.PARSER:
+    """ Constructs a parser to parse command line arguments
+
+    Returns:
+        argparse.PARSER: The parser object
+    """
     parser = argparse.ArgumentParser()
-    parser.add_argument("-v", help = "operate simulation in verbose mode", action = 'store_true')
-    parser.add_argument("-n", help = "the number of iterations to perform")
-    parser.add_argument("-s", help = "the number of stations to simulate")
-    parser.add_argument("-a", help = "the number of access points to simulate")
-    parser.add_argument("-p", help = "the probability of a station having a connection to an AP")
-    parser.add_argument("--protocol", help = "the handshake protocol to use")
-    parser.add_argument("--csv", help = "the .csv file to write results to")
+    parser.add_argument("-v", help="operate simulation in verbose mode", action='store_true')
+    parser.add_argument("-n", help="the number of iterations to perform")
+    parser.add_argument("-s", help="the number of stations to simulate")
+    parser.add_argument("-a", help="the number of access points to simulate")
+    parser.add_argument("-p", help="the probability of a station having a connection to an AP")
+    parser.add_argument("--protocol", help="the handshake protocol to use")
+    parser.add_argument("--csv", help="the .csv file to write results to")
     return parser
