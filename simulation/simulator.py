2d1b07efc2d3f370b5c275889eb1c535b38b926a
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2020-02-13 22:54:54 +0000

    Refactor classifier training function

--- simulation/simulator.py
+++ simulation/simulator.py
@@ -1,6 +1,9 @@
 import random
 from typing import List
+import numpy as np
 import progressbar as pb
+from sklearn.model_selection import KFold
+from sklearn.naive_bayes import GaussianNB
 from simulation.actors import Station, AccessPoint
 from simulation.frames import Frame
 
@@ -86,3 +89,26 @@ def simulate_secure_scan(stations: List[Station], aps: List[AccessPoint],
         print(request)
         print(response)
     return True
+
+def train_classifier(x: np.ndarray, y: np.ndarray, k: int) -> List[int]:
+    """ Trains a Naive Bayes classifier on `x` and `y` with `k` fold validation
+
+    Args:
+        x (np.ndarray): A numpy array of features
+        y (np.ndarray): A numpy array of labels
+        k (int):        The number of validation folds to perform
+
+    Returns:
+        List[int]: A list of accuracies for each fold of validation
+    """
+    acc = []
+    with pb.ProgressBar(max_value=k, term_width=100) as pbar:
+        progress = 0
+        for train_i, test_i in KFold(n_splits=k).split(x):
+            x_train, x_test = x[train_i], x[test_i]
+            y_train, y_test = y[train_i], y[test_i]
+            y_pred = GaussianNB().fit(x_train, y_train).predict(x_test)
+            acc.append((y_pred == y_test).sum() / len(y_pred))
+            progress += 1
+            pbar.update(progress)
+    return acc
