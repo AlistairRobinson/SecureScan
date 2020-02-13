27640abb83bfb096d1de38ce8e3f41589dcd6a3f
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2020-02-13 21:56:25 +0000

    Add simulator docstrings

--- simulation/simulator.py
+++ simulation/simulator.py
@@ -1,16 +1,76 @@
-from simulation.actors import Station, AccessPoint
-from simulation.frames import Frame, FrameType
+import random
 from typing import List
 import progressbar as pb
-import random
+from simulation.actors import Station, AccessPoint
+from simulation.frames import Frame
 
-def get_stations(s:int) -> List[Station]:
+def get_stations(s: int) -> List[Station]:
+    """ Returns a list of `s` Stations objects
+
+    Args:
+        s (int): The number of stations to construct
+
+    Returns:
+        List[Station]: The list of newly constructed stations
+    """
     return [Station(i) for i in pb.progressbar(range(s), term_width=100)]
 
-def get_access_points(a:int) -> List[AccessPoint]:
+def get_access_points(a: int) -> List[AccessPoint]:
+    """ Returns a list of `a` Access Points (APs)
+
+    Args:
+        a (int): The number of APs to construct
+
+    Returns:
+        List[Station]: The list of newly constructed APs
+    """
     return [AccessPoint(i) for i in pb.progressbar(range(a), term_width=100)]
 
-def simulate_secure_scan(stations:List[Station], aps:List[AccessPoint], history:List[Frame], v:bool=False) -> bool:
+def simulate_standard(stations: List[Station], aps: List[AccessPoint],
+                      history: List[Frame], v: bool = False) -> bool:
+    """ Simulates the WiFi handshake protocol, recorded in `history`
+
+    Args:
+        stations (List[Station]): The stations in the network
+        aps (List[AccessPoint]):  The APs in the network
+        history (List[Frame]):    The network history to append to
+        v (bool, optional):       Verbose mode, defaults to False
+
+    Returns:
+        bool: True if the handshake was successful, False otherwise
+    """
+    st = random.choice(stations)
+    ap = random.choice(aps)
+    valid = (ap.ssid in [i[0] for i in st.saved])
+    beacon = ap.send_beacon()
+    history.append(beacon)
+    if v:
+        print(beacon)
+    request = st.send_probe_request(beacon)
+    if valid:
+        response = ap.send_probe_response(request)
+        history.append(request)
+        history.append(response)
+        if v:
+            print(request)
+            print(response)
+        return True
+    assert request is None
+    return False
+
+def simulate_secure_scan(stations: List[Station], aps: List[AccessPoint],
+                         history: List[Frame], v: bool = False) -> bool:
+    """ Simulates the SecureScan protocol, recorded in `history`
+
+    Args:
+        stations (List[Station]): The stations in the network
+        aps (List[AccessPoint]):  The APs in the network
+        history (List[Frame]):    The network history to append to
+        v (bool, optional):       Verbose mode, defaults to False
+
+    Returns:
+        bool: True if the handshake was successful, False otherwise
+    """
     st = random.choice(stations)
     ap = random.choice(aps)
     valid = ((ap.ssid, ap.key.publickey().exportKey()) in st.saved)
@@ -26,24 +86,3 @@ def simulate_secure_scan(stations:List[Station], aps:List[AccessPoint], history:
         print(request)
         print(response)
     return True
-
-def simulate_standard(stations:List[Station], aps:List[AccessPoint], history:List[Frame], v:bool=False) -> bool:
-    st = random.choice(stations)
-    ap = random.choice(aps)
-    valid = (ap.ssid in [i[0] for i in st.saved])
-    beacon = ap.send_beacon()
-    history.append(beacon)
-    if v:
-        print(beacon)
-    request = st.send_probe_request(beacon)
-    if not valid:
-        assert request is None
-        return False
-    else:
-        response = ap.send_probe_response(request)
-        history.append(request)
-        history.append(response)
-        if v:
-            print(request)
-            print(response)
-        return True
