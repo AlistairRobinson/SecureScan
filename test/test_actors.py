a8c2676ad3df2a88c1a84cc8146101039fc212b6
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2020-03-03 09:23:21 +0000

    Add security attacks to unit testing framework

--- test/test_actors.py
+++ test/test_actors.py
@@ -12,13 +12,13 @@ def test_ap_constructor():
 
 def test_beacon():
     ap = AccessPoint()
-    beacon = ap.send_beacon()
+    _ = ap.send_beacon()
 
 def test_probe_request():
     ap = AccessPoint()
     st = Station()
     beacon = ap.send_beacon()
-    probe_request = st.send_probe_request(beacon)
+    _ = st.send_probe_request(beacon)
 
 def test_invalid_probe_response():
     ap = AccessPoint()
@@ -36,3 +36,24 @@ def test_valid_probe_response():
     probe_request = st.send_probe_request(beacon)
     probe_response = ap.send_probe_response(probe_request)
     assert st.verify_probe_response(probe_response)
+
+def test_karma():
+    adversary = AccessPoint()
+    ap = AccessPoint()
+    st = Station()
+    st.save_ap(ap)
+    beacon = ap.send_beacon()
+    probe_request = st.send_probe_request(beacon)
+    try:
+        probe_response = adversary.send_probe_response(probe_request)
+        assert not st.verify_probe_response(probe_response)
+    except ValueError:
+        return True
+
+def test_reverse_karma():
+    ap = AccessPoint()
+    st = Station()
+    st.save_ap(ap)
+    beacon = ap.send_beacon()
+    probe_request = st.send_probe_request(beacon)
+    assert not ap.ssid in str(probe_request.contents)
